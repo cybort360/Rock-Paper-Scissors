0acd77d467eeb239fa1204d8993d4a9c9fa1dbd4
cybort360 <cybort360@gmail.com>
2024-04-01 23:53:46 +0100

    rock paper scissors

---
+++ rps.js
@@ -0,0 +1,55 @@
+function getComputerChoice(){
+  const computerOptions = ['rock', 'paper', 'scissors'];
+  const random = Math.floor(Math.random() * computerOptions.length);
+  return computerOptions[random]
+}
+
+const playerSelection = prompt("Which do you want to choose? rock, paper or scissors").toLowerCase();
+const computerSelection = getComputerChoice()
+// console.log(cs);
+
+const game = (ps,cs) => {
+    switch (ps) {
+        case "rock":
+            if(cs == "paper"){
+                return `You lose, ${cs} beats ${ps}`;
+            }
+            if(cs == "scissors"){
+                return `You win, ${ps} beats ${cs}`;
+            }
+            if(cs == "rock"){
+                return "This is a tie!, Play again?"
+            }
+            break;
+        case "paper":
+            if(cs == "scissors"){
+                return `You lose, ${cs} beats ${ps}`;
+            }
+            if(cs =="rock"){
+                return `You win, ${ps} beats ${cs}`;
+            }
+            if(cs == "paper"){
+                return "This is a tie!, Play again?"
+            }
+            break;
+        case "scissors":
+            if(cs == "rock"){
+                return `You lose, ${cs} beats ${ps}`;
+            }
+            if(cs == "paper"){
+                return `You win, ${ps} beats ${cs}`;
+            }
+            if(cs =="scissors"){
+                return "This is a tie!, Play again?"
+            }
+            break;
+        // default:
+        //     if(ps == cs){
+        //         return "This is a tie!, Play again?" ;
+        //     }
+        //     break;
+    }
+}
+
+console.log(game(playerSelection,computerSelection));
+
